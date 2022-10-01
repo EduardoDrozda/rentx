49b8b8f2bce41aebc23e60b7ed91986df167153d
Eduardo Drozda <Edu_Drozda@hotmail.com>
2022-10-01 15:30:28 -0300

    feat: config debug in vscode

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
